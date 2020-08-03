ab366b4b3c8cab1f503829541d620c0af6df091d
Wesley rolim <rolim.r@icloud.com>
2020-08-03 02:57:30 -0300

    primeiro commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        public IActionResult ContatoAcao()
+        {
+            string nome = HttpContext.Request.Form["nome"];
+            string email = HttpContext.Request.Form["email"];
+            string texto = HttpContext.Request.Form["texto"];
+
+            return new ContentResult() { Content = String.Format("Enviado com Sucesso <br />Nome: {0}  <br /> Email: {1}  <br /> Texto: {2}", nome, email, texto), ContentType = "text/html" };
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,33 @@
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller //todo controller herda da classe Controller
+    {
+        /*
+         *ActionResult
+         *IActionResult
+         */
+        public ActionResult Visualizar() //na classe controller todo metodo tem que ser ActionResult 
+        {
+            Produto produto = GetProduto();
+
+
+            return View(produto);
+            //return new ContentResult() { Content = "<h3>Produto -> Visualizar</h3>", ContentType = "text/html" };
+        }
+
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "Xbox One X",
+                Descricao = "Jogue em 4k true",
+                Valor = 2500.00M
+            };
+        }
+    }
+}
