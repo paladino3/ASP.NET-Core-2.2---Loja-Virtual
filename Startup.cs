112d96259871b943f41d8dc5c44e73830d802454
Wesley Rolim Simoes <39566550+paladino3@users.noreply.github.com>
2020-08-03 02:22:01 -0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LojaVirtual
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseDefaultFiles(); // direciona automaticamente para o index.html
+            app.UseStaticFiles(); // usa as classe staticas de wwwroot, como bootstrap, css etc
+            app.UseCookiePolicy();
+
+
+            /*
+             * https://www.site.com.br -> Qual controlador? (gestao) -> Rotas
+             * https://www.site.com.br/Produtos/Visualizar/MouseRazerZk
+             * https://www.site.com.br/Produtos/Visualizar -> Listagem de todos os produtos (? opcional)
+             *
+             * https://www.site.com.br -> https://www.site.com.br/Index
+             * https://www.site.com.br -> https://www.site.com.br/Produto/Index
+            */
+
+
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+           
+        }
+    }
+}
