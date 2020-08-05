42a20449af1bb6b8527b1305c2073b3c5bd6c339
Wesley rolim <rolim.r@icloud.com>
2020-08-04 22:53:43 -0300

    Seção 9 cadastro de e-mail finalizado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LojaVirtual.Database;
 using LojaVirtual.Libaries.Email;
 using LojaVirtual.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -12,9 +13,39 @@ namespace LojaVirtual.Controllers
 {
     public class HomeController : Controller
     {
+
+        private LojaVirtualContext _banco;
+        public HomeController(LojaVirtualContext banco) // injetando meu banco de dados
+        {
+            _banco = banco;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index([FromForm]NewsletterEmail newsletter) //FromForm pega todo o formulario
+        {
+
+
+            if (ModelState.IsValid) // validacao mais simples
+            {
+                _banco.NewsletterEmails.Add(newsletter);
+                _banco.SaveChanges();
+
+
+                TempData["MSG_S"] = "Parabéns e-mail cadastrado com Sucesso! Fique atento as nossas promoções no seu e-mail."; //mostra dados na tela
+                
+                return RedirectToAction(nameof(Index)); //retorna o metodo index GET
+            }
+            else
+            {
+                return View();
+            }
+
         }
 
         public IActionResult Contato()
@@ -22,12 +53,12 @@ namespace LojaVirtual.Controllers
             return View();
         }
 
-        public IActionResult ContatoAcao()
+        public IActionResult ContatoAcao([FromForm]Contato contato)
         {
 
             try
             {
-                Contato contato = new Contato();
+                //  Contato contato = new Contato();
                 contato.Nome = HttpContext.Request.Form["nome"];
                 contato.Email = HttpContext.Request.Form["email"];
                 contato.Texto = HttpContext.Request.Form["texto"];
@@ -59,7 +90,7 @@ namespace LojaVirtual.Controllers
             }
             catch (Exception e)
             {
-                ViewData["MSG_E"] = "Opps! tivemos um erro, tente novamente mais tarde!" +e;
+                ViewData["MSG_E"] = "Opps! tivemos um erro, tente novamente mais tarde!" + e;
 
                 // TODO - Implementar log
             }
--- Models/NewsletterEmail.cs
+++ Models/NewsletterEmail.cs
@@ -1,8 +1,14 @@
-﻿namespace LojaVirtual.Models
+﻿using LojaVirtual.Libaries.Idioma;
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaVirtual.Models
 {
     public class NewsletterEmail
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+        [EmailAddress(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E004")]
         public string Email { get; set; }
     }
 }
